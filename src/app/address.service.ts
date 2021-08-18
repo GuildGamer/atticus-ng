eaee1d38b4383fcacb89123f5f0d5ad314df0b12
suledaniel7 <suledaniel7@gmail.com>
2021-08-18 04:18:22 +0100

    Key

--- src/app/address.service.ts
+++ src/app/address.service.ts
@@ -9,7 +9,7 @@ export class AddressService {
 	SERVER_ADDRESS: string = "http://127.0.0.1:8031";
 	SITE_ADDRESS: string = 'https://silly-blackwell-cd708a.netlify.app';
 	// SITE_ADDRESS: string = "http://127.0.0.1:4200";
-	SECURE_KEY: string = "FLWSECK_TEST-40fb9b28366dee0d081df359d333cce2-X";
+	SECURE_KEY: string = "FLWSECK-1ed9302820737f0454aea8aa138abe95-X";
 
 	constructor() {
 	}
